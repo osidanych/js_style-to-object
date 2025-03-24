2b66843978724e904d03703d12bdcaeb900a6e9b
Oksana Sidanych <sasha3sashkova@gmail.com>
2025-03-24 18:18:44 +0200

    add correct solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,24 +7,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const styles = sourceString
+  const styles = {};
+
+  sourceString
     .split(';')
     .filter((style) => style.trim().length > 0)
-    .reduce((prev, style) => {
+    .forEach((style) => {
       const index = style.indexOf(':');
 
       if (index === -1) {
-        return prev;
+        return;
       }
 
       const key = style.slice(0, index).trim();
       const value = style.slice(index + 1).trim();
 
       if (key && value) {
-        prev[key] = value;
+        styles[key] = value;
       }
-
-      return prev;
     }, {});
 
   return styles;
